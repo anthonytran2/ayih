f703c8bd14b424e1c092aa244957199ec96fe6ee
anthonytran2 <anthonytran2@users.noreply.github.com>
2018-09-09 13:48:37 -0700

    Windows display support added.

--- adv_hiya.cpp
+++ adv_hiya.cpp
@@ -7,7 +7,6 @@
 	PROGRAM NAME: ayih
 */
 
-
 #include <iostream>
 #include <string>
 #include <stdio.h>
@@ -24,6 +23,15 @@
 #define WHITE 7
 #define BLUE 4
 #define YELLOW 3 
+
+//Windows display support.
+#ifdef _WIN32 
+static const bool windows = true;
+#include<windows.h>
+HANDLE hConsole;
+#elif __linux__ || __unix__
+static const bool windows = false;
+#endif
 
 using namespace std;
 
@@ -988,9 +996,30 @@ void printBoard(){
 }
 
 void textColor(int attr, int fg, int bg){
-  char command[13];
-  sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
-  printf("%s", command);
+    if(windows == false) {  
+        char command[13];
+        sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+        printf("%s", command);
+    } else if(windows == true) {
+        #ifdef _WIN32 
+        int k = 0;
+        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        if(bg == BLACK && fg == GREEN)
+          k = 10;
+        else if(bg == BLACK && fg == RED)
+          k = 12;
+        else if(bg == BLACK && fg == YELLOW)
+          k = 14;
+        else if(bg == BLACK && fg == WHITE)
+          k = 15;
+        else if(bg == BLUE && fg == RED)
+          k = 28;
+        else if(bg == BLUE && fg == WHITE)
+          k = 31;
+      
+        SetConsoleTextAttribute(hConsole, k);
+        #endif
+    }
 }
 
 void isGameOver(std::string player){
